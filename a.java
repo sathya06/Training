865ba1a2f336c8172054f9b7dea2c4bcacb34079
training_C2D.02.11 <training_C2D.02.11@accenture.com>
2018-08-09 14:45:43 +0530

    Added about git

--- a.java
+++ a.java
@@ -1,2 +1,6 @@
 hello world
 it is a basic program
+about git
+Git is a revision control system, a tool to manage your source code history.
+GitHub is a hosting service for Git repositories.
+So they are not the same thing: Git is the tool, GitHub is the service for projects that use Git.
