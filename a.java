fb6631a9242308765959c0efddda304e564f2cf6
training_C2D.02.11 <training_C2D.02.11@accenture.com>
2018-08-09 14:46:43 +0530

    Added about repository

--- a.java
+++ a.java
@@ -4,3 +4,8 @@ about git
 Git is a revision control system, a tool to manage your source code history.
 GitHub is a hosting service for Git repositories.
 So they are not the same thing: Git is the tool, GitHub is the service for projects that use Git.
+about repositpry
+stores metadata for a set of files or directory structure.
+A historical record of changes in the repository.
+A set of commit objects.
+A set of references to commit objects, called heads.
