ae3f42548856f2d9bb5727f88230400860193264
training_C2D.02.11 <training_C2D.02.11@accenture.com>
2018-08-09 14:59:24 +0530

    added after reset

--- a.java
+++ a.java
@@ -9,3 +9,7 @@ stores metadata for a set of files or directory structure.
 A historical record of changes in the repository.
 A set of commit objects.
 A set of references to commit objects, called heads.
+A branch in Git is simply a lightweight movable pointer to one of these commits. The default branch name in Git is master. As you start making commits, you’re given a master branch that points to the last commit you made. Every time you commit, the master branch pointer moves forward automatically
+git branch -----
+This creates a new pointer to the same commit you’re currently on.
+hello this going to commit after reset
